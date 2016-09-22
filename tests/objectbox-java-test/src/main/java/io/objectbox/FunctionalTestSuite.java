3bc0b95c5a90253726211dba6f000c17a126abab
Markus <noreply@objectbox.io>
2016-09-22 14:22:10 +0200

    minor

--- tests/objectbox-java-test/src/main/java/io/objectbox/FunctionalTestSuite.java
+++ tests/objectbox-java-test/src/main/java/io/objectbox/FunctionalTestSuite.java
@@ -5,6 +5,12 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({BoxStoreTest.class, CursorTest.class, CursorBytesTest.class, TransactionTest.class, BoxTest.class})
+@SuiteClasses({
+        BoxStoreTest.class,
+        CursorTest.class,
+        CursorBytesTest.class,
+        TransactionTest.class,
+        BoxTest.class
+})
 public class FunctionalTestSuite {
 }
