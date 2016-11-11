c0a80fd82bbb7d44ecec70fa2be4808b4d185468
Markus <noreply@objectbox.io>
2016-11-11 15:25:13 +0100

    added idAssignable flag

--- objectbox-java-api/src/main/java/io/objectbox/annotation/Id.java
+++ objectbox-java-api/src/main/java/io/objectbox/annotation/Id.java
@@ -13,7 +13,14 @@ import java.lang.annotation.Target;
 public @interface Id {
     /**
      * Specifies that id should increase monotonic without reusing IDs. This decreases performance a little bit for
-     * putting new objects (inserts).
+     * putting new objects (inserts) because the state needs to be persisted. Gaps between two IDs may still occur,
+     * e.g. if inserts are rollbacked.
      */
     boolean monotonic() default false;
+
+    /**
+     * Allows IDs to be assigned by the developer. This may make sense for using IDs originating somewhere else, e.g.
+     * from the server.
+     */
+    boolean assignable() default false;
 }
