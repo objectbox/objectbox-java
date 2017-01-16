4834e8fbaaa26aa24131e0cab8ef5f86ccfdc38b
Markus <noreply@objectbox.io>
2017-01-16 17:17:00 +0100

    setting some years to 2017

--- objectbox-java/src/main/java/io/objectbox/Property.java
+++ objectbox-java/src/main/java/io/objectbox/Property.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Markus Junginger
+ * Copyright (C) 2017 Markus Junginger
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
