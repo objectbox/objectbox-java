5e6bb2c5cbca1b678def2020b50c9c9a20e24bfe
Markus <noreply@objectbox.io>
2017-02-16 20:49:06 +0100

    remove obsolete ObjectClassObserver.java and Subscription (android)

--- objectbox-java/src/main/java/io/objectbox/ObjectClassObserver.java
+++
@@ -1,8 +0,0 @@
-package io.objectbox;
-
-/**
- * Called when objects of a certain type were put or removed in the last committed transaction.
- */
-public interface ObjectClassObserver<T> {
-    void onChanges(Class<T> objectClass);
-}
