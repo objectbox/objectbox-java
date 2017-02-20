69d0d77e146230d2cbcf12cd8001ac87b9122d55
Markus <noreply@objectbox.io>
2017-02-20 10:21:49 +0100

    AndroidScheduler.java: static method instead of field, JavaDoc

---
+++ objectbox-java/src/main/java/io/objectbox/reactive/Schedulers.java
@@ -0,0 +1,12 @@
+package io.objectbox.reactive;
+
+// How to get to BoxStore thread pool?
+class Schedulers {
+    static Scheduler async() {
+        return null;
+    }
+
+    static Scheduler singleThreadSequential() {
+        return null;
+    }
+}
