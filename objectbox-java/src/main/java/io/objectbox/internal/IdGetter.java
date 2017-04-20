d684ac05ea84fd6cd6d3171234393a0bffcbad65
Markus <noreply@objectbox.io>
2017-04-20 19:42:24 +0200

    Use IdGetter instead of dummy Cursor

--- objectbox-java/src/main/java/io/objectbox/internal/IdGetter.java
+++ objectbox-java/src/main/java/io/objectbox/internal/IdGetter.java
@@ -1,6 +1,6 @@
 package io.objectbox.internal;
 
 public interface IdGetter<T> {
-    /** Given object must be non-null and have a valid ID */
+    /** Given object must be non-null and have a non-null ID. */
     long getId(T object);
 }
