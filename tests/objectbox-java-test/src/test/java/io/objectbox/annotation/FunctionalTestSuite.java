a4b89d32cdbc1579505d41bbf28a2a2bca3b6eee
Markus <noreply@objectbox.io>
2017-05-12 22:43:12 +0200

    FunctionalTestSuite.java: Duplicate for gradle

---
+++ tests/objectbox-java-test/src/test/java/io/objectbox/annotation/FunctionalTestSuite.java
@@ -0,0 +1,39 @@
+package io.objectbox.annotation;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import io.objectbox.BoxStoreBuilderTest;
+import io.objectbox.BoxStoreTest;
+import io.objectbox.BoxTest;
+import io.objectbox.CursorBytesTest;
+import io.objectbox.CursorTest;
+import io.objectbox.ObjectClassObserverTest;
+import io.objectbox.TransactionTest;
+import io.objectbox.index.IndexReaderRenewTest;
+import io.objectbox.query.LazyListTest;
+import io.objectbox.query.QueryObserverTest;
+import io.objectbox.query.QueryTest;
+import io.objectbox.relation.RelationTest;
+import io.objectbox.relation.ToOneTest;
+
+/** Duplicate for gradle */
+@RunWith(Suite.class)
+@SuiteClasses({
+        BoxTest.class,
+        BoxStoreTest.class,
+        BoxStoreBuilderTest.class,
+        CursorTest.class,
+        CursorBytesTest.class,
+        LazyListTest.class,
+        IndexReaderRenewTest.class,
+        ObjectClassObserverTest.class,
+        QueryObserverTest.class,
+        QueryTest.class,
+        RelationTest.class,
+        ToOneTest.class,
+        TransactionTest.class,
+})
+public class FunctionalTestSuite {
+}
