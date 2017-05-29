736191809a3945e954dafd5cb581e29dc8cd8800
Markus <noreply@objectbox.io>
2017-05-29 22:38:32 +0200

    test nonArg constructor with more types

--- tests/objectbox-java-test/src/main/java/io/objectbox/NonArgConstructorTest.java
+++ tests/objectbox-java-test/src/main/java/io/objectbox/NonArgConstructorTest.java
@@ -3,6 +3,8 @@ package io.objectbox;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import io.objectbox.ModelBuilder.EntityBuilder;
 import io.objectbox.model.EntityFlags;
 
@@ -29,12 +31,29 @@ public class NonArgConstructorTest extends AbstractObjectBoxTest {
     public void testPutAndGet() {
         TestEntity entity = new TestEntity();
         entity.setSimpleInt(1977);
+        entity.setSimpleBoolean(true);
+        entity.setSimpleByte((byte) 42);
+        byte[] bytes = {77, 78};
+        entity.setSimpleByteArray(bytes);
+        entity.setSimpleDouble(3.141);
+        entity.setSimpleFloat(3.14f);
+        entity.setSimpleLong(789437444354L);
+        entity.setSimpleShort((short) 233);
+        entity.setSimpleString("foo");
         long key = box.put(entity);
 
         TestEntity entityRead = box.get(key);
         assertNotNull(entityRead);
         assertTrue(entityRead.noArgsConstructorCalled);
         assertEquals(1977, entityRead.getSimpleInt());
+        assertTrue(entityRead.getSimpleBoolean());
+        assertEquals(42, entityRead.getSimpleByte());
+        assertEquals(233, entityRead.getSimpleShort());
+        assertEquals(789437444354L, entityRead.getSimpleLong());
+        assertEquals(3.14f, entityRead.getSimpleFloat(), 0.000001f);
+        assertEquals(3.141f, entityRead.getSimpleDouble(), 0.000001);
+        assertTrue(Arrays.equals(bytes, entityRead.getSimpleByteArray()));
+        assertEquals("foo", entityRead.getSimpleString());
     }
 
 }
