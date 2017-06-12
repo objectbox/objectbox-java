c636fde4a8ef2a7b4b00dad11f0d307d15924fbb
Markus <noreply@objectbox.io>
2017-06-12 14:45:09 +0200

    minor: suppress warning

--- objectbox-kotlin/src/main/kotlin/io/objectbox/kotlin/Extensions.kt
+++ objectbox-kotlin/src/main/kotlin/io/objectbox/kotlin/Extensions.kt
@@ -6,4 +6,5 @@ import kotlin.reflect.KClass
 
 inline fun <reified T> BoxStore.boxFor(): Box<T> = boxFor(T::class.java)
 
+@Suppress("NOTHING_TO_INLINE")
 inline fun <T : Any> BoxStore.boxFor(clazz: KClass<T>): Box<T> = boxFor(clazz.java)
