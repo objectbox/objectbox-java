d185f61da912f4967216c1a082d05b488509ef32
Markus <noreply@objectbox.io>
2017-07-17 13:42:37 +0100

    inValues method alias for Kotlin

--- objectbox-kotlin/src/main/kotlin/io/objectbox/kotlin/Extensions.kt
+++ objectbox-kotlin/src/main/kotlin/io/objectbox/kotlin/Extensions.kt
@@ -2,9 +2,19 @@ package io.objectbox.kotlin
 
 import io.objectbox.Box
 import io.objectbox.BoxStore
+import io.objectbox.Property
+import io.objectbox.query.QueryBuilder
 import kotlin.reflect.KClass
 
 inline fun <reified T> BoxStore.boxFor(): Box<T> = boxFor(T::class.java)
 
 @Suppress("NOTHING_TO_INLINE")
 inline fun <T : Any> BoxStore.boxFor(clazz: KClass<T>): Box<T> = boxFor(clazz.java)
+
+/** An alias for the "in" method, which is a reserved keyword in Kotlin. */
+inline fun <reified T> QueryBuilder<T>.inValues(property: Property, values: LongArray): QueryBuilder<T>?
+        = `in`(property, values)
+
+/** An alias for the "in" method, which is a reserved keyword in Kotlin. */
+inline fun <reified T> QueryBuilder<T>.inValues(property: Property, values: IntArray): QueryBuilder<T>?
+        = `in`(property, values)
