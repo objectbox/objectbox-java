cd148bf682437a8e41402e9715756265ffc3a38b
Markus <noreply@objectbox.io>
2017-07-29 17:32:12 +0100

    prep RelationInfo for relationId

--- objectbox-java/src/main/java/io/objectbox/relation/RelationInfo.java
+++ objectbox-java/src/main/java/io/objectbox/relation/RelationInfo.java
@@ -18,10 +18,15 @@ public class RelationInfo<TARGET> implements Serializable {
 
     public final EntityInfo sourceInfo;
     public final EntityInfo<TARGET> targetInfo;
+
+    /** For relations based on a target ID property (null for stand-alone relations). */
     public final Property targetIdProperty;
 
     /** For ToMany relations based on backlinks (null otherwise). */
     public final ToOneGetter toOneGetter;
+
+    /** For stand-alone to-many relations (0 otherwise). */
+    public final int relationId;
 
     public RelationInfo(EntityInfo sourceInfo, EntityInfo<TARGET> targetInfo, Property targetIdProperty) {
         this(sourceInfo, targetInfo, targetIdProperty, null);
@@ -33,6 +38,15 @@ public class RelationInfo<TARGET> implements Serializable {
         this.targetInfo = targetInfo;
         this.targetIdProperty = targetIdProperty;
         this.toOneGetter = toOneGetter;
+        relationId = 0;
+    }
+
+    public RelationInfo(EntityInfo sourceInfo, EntityInfo<TARGET> targetInfo, int relationId) {
+        this.sourceInfo = sourceInfo;
+        this.targetInfo = targetInfo;
+        this.relationId = relationId;
+        this.targetIdProperty = null;
+        this.toOneGetter = null;
     }
 }
 
