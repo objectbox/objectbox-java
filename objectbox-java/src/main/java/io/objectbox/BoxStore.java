af1b15ae91959e19ddb34e21a9fdc9b0da1db4af
Markus <noreply@objectbox.io>
2017-09-10 15:39:11 +0100

    BoxStore.java: V1.0.1-2017-09-10

--- objectbox-java/src/main/java/io/objectbox/BoxStore.java
+++ objectbox-java/src/main/java/io/objectbox/BoxStore.java
@@ -130,7 +130,7 @@ public class BoxStore implements Closeable {
     static native int nativeCleanStaleReadTransactions(long store);
 
     public static String getVersion() {
-        return "1.0.0-2017-09-02";
+        return "1.0.1-2017-09-10";
     }
 
     private final File directory;
