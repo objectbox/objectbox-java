ae8c18ec8563986aa55818031fa303b6a56d113b
greenrobot Team <opensource+team@greenrobot>
2017-10-30 11:06:31 +0100

    Revert "Gradle: Drop FunctionalTestSuite, instead add main source set to tests."
    This reverts commit 38e8214

---
+++ tests/objectbox-java-test/src/test/java/io/objectbox/annotation/FunctionalTestSuite.java
@@ -0,0 +1,43 @@
+package io.objectbox.annotation;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import io.objectbox.BoxStoreBuilderTest;
+import io.objectbox.BoxStoreTest;
+import io.objectbox.BoxTest;
+import io.objectbox.CursorBytesTest;
+import io.objectbox.CursorTest;
+import io.objectbox.NonArgConstructorTest;
+import io.objectbox.ObjectClassObserverTest;
+import io.objectbox.TransactionTest;
+import io.objectbox.index.IndexReaderRenewTest;
+import io.objectbox.query.LazyListTest;
+import io.objectbox.query.QueryObserverTest;
+import io.objectbox.query.QueryTest;
+import io.objectbox.relation.RelationEagerTest;
+import io.objectbox.relation.RelationTest;
+import io.objectbox.relation.ToOneTest;
+
+/** Duplicate for gradle */
+@RunWith(Suite.class)
+@SuiteClasses({
+        BoxTest.class,
+        BoxStoreTest.class,
+        BoxStoreBuilderTest.class,
+        CursorTest.class,
+        CursorBytesTest.class,
+        LazyListTest.class,
+        NonArgConstructorTest.class,
+        IndexReaderRenewTest.class,
+        ObjectClassObserverTest.class,
+        QueryObserverTest.class,
+        QueryTest.class,
+        RelationTest.class,
+        RelationEagerTest.class,
+        ToOneTest.class,
+        TransactionTest.class,
+})
+public class FunctionalTestSuite {
+}
