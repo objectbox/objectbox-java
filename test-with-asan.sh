8156ff0495f2496f5b06f41bda887306ba274bb8
Markus <markus@greenrobot>
2017-11-19 11:06:32 +0100

    added test-with-asan.sh

---
+++ test-with-asan.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+
+if [ -z "$ASAN_LIB_SO" ]; then
+    export ASAN_LIB_SO=/usr/lib/clang/5.0.1/lib/linux/libclang_rt.asan-x86_64.so
+fi
+
+if [ -z "$ASAN_SYMBOLIZER_PATH" ]; then
+    export ASAN_SYMBOLIZER_PATH=/usr/lib/llvm-5.0/bin/llvm-symbolizer
+fi
+
+if [ -z "$ASAN_OPTIONS" ]; then
+    export ASAN_OPTIONS="detect_leaks=0"
+fi
+
+echo "ASAN_LIB_SO: $ASAN_LIB_SO"
+echo "ASAN_SYMBOLIZER_PATH: $ASAN_SYMBOLIZER_PATH"
+echo "ASAN_OPTIONS: $ASAN_OPTIONS"
+ls -l $ASAN_LIB_SO
+ls -l $ASAN_SYMBOLIZER_PATH
+
+LD_PRELOAD=${ASAN_LIB_SO} ./gradlew -Dorg.gradle.daemon=false --stacktrace test
