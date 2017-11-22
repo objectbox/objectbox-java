1f3262dfc2c2a7484aaa9c04857d821894558cbd
Markus <noreply@objectbox.io>
2017-11-22 14:40:05 +0100

    added link to BoxStore.diagnose() for DbMaxReadersExceededException

--- objectbox-java/src/main/java/io/objectbox/exception/DbMaxReadersExceededException.java
+++ objectbox-java/src/main/java/io/objectbox/exception/DbMaxReadersExceededException.java
@@ -1,10 +1,15 @@
 package io.objectbox.exception;
 
+import io.objectbox.BoxStore;
+
 /**
  * Thrown when the maximum of readers (read transactions) was exceeded.
  * Verify that you run a reasonable amount of threads only.
+ * <p>
  * If you intend to work with a very high number of threads (>100), consider increasing the number of maximum readers
  * using {@link io.objectbox.BoxStoreBuilder#maxReaders(int)}.
+ * <p>
+ * For debugging issues related to this exception, check {@link BoxStore#diagnose()}.
  */
 public class DbMaxReadersExceededException extends DbException {
     public DbMaxReadersExceededException(String message) {
