e45d51300ccba5fdc2ecae3b44dc8994e58bd036
Markus <noreply@objectbox.io>
2017-11-27 16:18:15 +0100

    BoxStore version 1.3.0-2017-11-27

--- objectbox-java/src/main/java/io/objectbox/BoxStore.java
+++ objectbox-java/src/main/java/io/objectbox/BoxStore.java
@@ -138,7 +138,7 @@ public class BoxStore implements Closeable {
     public static native boolean isObjectBrowserAvailable();
 
     public static String getVersion() {
-        return "1.2.2-2017-11-22";
+        return "1.3.0-2017-11-27";
     }
 
     private final File directory;
