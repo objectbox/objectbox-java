8853d0541e5efdf5a88172b60dd1e3b27bb67797
Markus <noreply@objectbox.io>
2017-12-18 12:37:00 +0100

    PropertyQuery: JavaDocs, check string type

--- objectbox-java/src/main/java/io/objectbox/query/PropertyQuery.java
+++ objectbox-java/src/main/java/io/objectbox/query/PropertyQuery.java
@@ -6,7 +6,11 @@ import java.util.concurrent.Callable;
 import io.objectbox.InternalAccess;
 import io.objectbox.Property;
 
-/** Query for a specific property; create using {@link Query#property(Property)}. */
+/**
+ * Query for a specific property; create using {@link Query#property(Property)}.
+ * Note: Property values do currently not consider any order defined for the main {@link Query} object
+ * (subject to change in a future version).
+ */
 @SuppressWarnings("WeakerAccess") // WeakerAccess: allow inner class access without accessor
 public class PropertyQuery {
     final Query query;
@@ -19,12 +23,25 @@ public class PropertyQuery {
         this.property = property;
     }
 
+    /**
+     * Only distinct values should be returned (e.g. 1,2,3 instead of 1,1,2,3,3,3).
+     * <p>
+     * Note: strings default to case-insensitive comparision;
+     * to change that call {@link #distinct(QueryBuilder.StringOrder)}.
+     */
     public PropertyQuery distinct() {
         distinct = true;
         return this;
     }
 
+    /**
+     * For string properties you can specify {@link io.objectbox.query.QueryBuilder.StringOrder#CASE_SENSITIVE} if you
+     * want to have case sensitive distinct values (e.g. returning "foo","Foo","FOO" instead of "foo").
+     */
     public PropertyQuery distinct(QueryBuilder.StringOrder stringOrder) {
+        if (property.type != String.class) {
+            throw new RuntimeException("Reserved for string properties, but got " + property);
+        }
         distinct = true;
         noCaseIfDistinct = stringOrder == QueryBuilder.StringOrder.CASE_INSENSITIVE;
         return this;
@@ -33,7 +50,11 @@ public class PropertyQuery {
     /**
      * Find the values for the given string property for objects matching the query.
      * <p>
-     * Note: this will list all strings (except null values), which may contain duplicates.
+     * Note: null values are excluded from results.
+     * <p>
+     * Note: results are not guaranteed to be in any particular order.
+     * <p>
+     * See also: {@link #distinct}, {@link #distinct(QueryBuilder.StringOrder)}
      *
      * @return Found strings
      */
