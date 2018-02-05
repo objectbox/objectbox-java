99a402b57d7fe848ca3409788ab7b3dad7e10be6
Markus <noreply@objectbox.io>
2018-02-05 23:54:15 +0100

    extended testDebugCursor_seekOrNext_get()

--- tests/objectbox-java-test/src/main/java/io/objectbox/DebugCursorTest.java
+++ tests/objectbox-java-test/src/main/java/io/objectbox/DebugCursorTest.java
@@ -24,27 +24,45 @@ import java.util.Arrays;
 
 import io.objectbox.internal.DebugCursor;
 
+import static org.junit.Assert.assertEquals;
+
 public class DebugCursorTest extends AbstractObjectBoxTest {
 
     @Test
     public void testDebugCursor_seekOrNext_get() {
-        TestEntity entity = putTestEntity("foobar", 42);
+        TestEntity entity1 = putTestEntity("foo", 23);
+        TestEntity entity2 = putTestEntity("bar", 42);
 
         Transaction transaction = store.beginReadTx();
         DebugCursor debugCursor = DebugCursor.create(transaction);
 
+        // seek to first entity
         ByteBuffer bytes = ByteBuffer.allocate(8);
         int partitionPrefix = (6 << 26) | (1 << 2);
-        bytes.putInt(partitionPrefix);
-        bytes.putInt(0);
-        byte[] entityKey = debugCursor.seekOrNext(bytes.array());
-        System.out.println(Arrays.toString(entityKey));
-        Assert.assertNotNull(entityKey);
-        Assert.assertEquals(8, entityKey.length);
-        Assert.assertEquals((byte) entity.getId(), entityKey[entityKey.length - 1]);
+        bytes.putInt(partitionPrefix).putInt(0);
+        byte[] entity1Key = debugCursor.seekOrNext(bytes.array());
+        System.out.println(Arrays.toString(entity1Key));
+        Assert.assertNotNull(entity1Key);
+        assertEquals(8, entity1Key.length);
 
-        byte[] value = debugCursor.get(entityKey);
-        Assert.assertNotNull(value);
+        // check key 1
+        bytes.rewind();
+        bytes.put(entity1Key);
+        bytes.flip();
+        assertEquals(partitionPrefix, bytes.getInt());
+        assertEquals((int) entity1.getId(), bytes.getInt());
+
+        // get value 1
+        byte[] value1 = debugCursor.get(entity1Key);
+        Assert.assertNotNull(value1);
+        Assert.assertTrue(value1.length > 40);
+
+        // get value 2
+        bytes.rewind();
+        bytes.putInt(partitionPrefix).putInt((int) entity2.getId());
+        byte[] value2 = debugCursor.get(bytes.array());
+        Assert.assertNotNull(value2);
+        Assert.assertTrue(value2.length > 40);
 
         debugCursor.close();
         transaction.abort();
