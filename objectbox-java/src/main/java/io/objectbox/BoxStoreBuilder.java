05cebe9d19a0940d4edaaa38571aa23cb7680ac2
Markus <noreply@objectbox.io>
2018-03-08 13:29:19 +0100

    initialDbFile provisioning

--- objectbox-java/src/main/java/io/objectbox/BoxStoreBuilder.java
+++ objectbox-java/src/main/java/io/objectbox/BoxStoreBuilder.java
@@ -16,7 +16,16 @@
 
 package io.objectbox;
 
+import org.greenrobot.essentials.io.IoUtils;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
@@ -26,6 +35,7 @@ import javax.annotation.Nullable;
 
 import io.objectbox.annotation.apihint.Experimental;
 import io.objectbox.annotation.apihint.Internal;
+import io.objectbox.exception.DbException;
 import io.objectbox.ideasonly.ModelUpdate;
 
 /**
@@ -80,6 +90,7 @@ public class BoxStoreBuilder {
     TxCallback failedReadTxAttemptCallback;
 
     final List<EntityInfo> entityInfoList = new ArrayList<>();
+    private Factory<InputStream> initialDbFileFactory;
 
     /** Not for application use. */
     public static BoxStoreBuilder createDebugWithoutModel() {
@@ -306,6 +317,22 @@ public class BoxStoreBuilder {
         return this;
     }
 
+    @Experimental
+    public BoxStoreBuilder initialDbFile(final File initialDbFile) {
+        return initialDbFile(new Factory<InputStream>() {
+            @Override
+            public InputStream provide() throws FileNotFoundException {
+                return new FileInputStream(initialDbFile);
+            }
+        });
+    }
+
+    @Experimental
+    public BoxStoreBuilder initialDbFile(Factory<InputStream> initialDbFileFactory) {
+        this.initialDbFileFactory = initialDbFileFactory;
+        return this;
+    }
+
     /**
      * Builds a {@link BoxStore} using any given configuration.
      */
@@ -314,7 +341,33 @@ public class BoxStoreBuilder {
             name = dbName(name);
             directory = getDbDir(baseDirectory, name);
         }
+        checkProvisionInitialDbFile();
         return new BoxStore(this);
+    }
+
+    private void checkProvisionInitialDbFile() {
+        if (initialDbFileFactory != null) {
+            String dataDir = BoxStore.getCanonicalPath(directory);
+            File file = new File(dataDir, "data.mdb");
+            if (!file.exists()) {
+                InputStream in = null;
+                OutputStream out = null;
+                try {
+                    in = initialDbFileFactory.provide();
+                    if (in == null) {
+                        throw new DbException("Factory did not provide a resource");
+                    }
+                    in = new BufferedInputStream(in);
+                    out = new BufferedOutputStream(new FileOutputStream(file));
+                    IoUtils.copyAllBytes(in, out);
+                } catch (Exception e) {
+                    throw new DbException("Could not provision initial data file", e);
+                } finally {
+                    IoUtils.safeClose(out);
+                    IoUtils.safeClose(in);
+                }
+            }
+        }
     }
 
     static File getDbDir(@Nullable File baseDirectoryOrNull, @Nullable String nameOrNull) {
