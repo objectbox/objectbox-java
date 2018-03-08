9c28171e73b9ad269f549e7c67bb8ec2d4e11040
Markus <noreply@objectbox.io>
2018-03-08 15:46:07 +0100

    1.4.4-2018-03-08

--- objectbox-java/src/main/java/io/objectbox/BoxStore.java
+++ objectbox-java/src/main/java/io/objectbox/BoxStore.java
@@ -59,7 +59,7 @@ import io.objectbox.reactive.SubscriptionBuilder;
 @ThreadSafe
 public class BoxStore implements Closeable {
 
-    private static final String VERSION = "1.4.4-2018-03-07";
+    private static final String VERSION = "1.4.4-2018-03-08";
     private static BoxStore defaultStore;
 
     private static final Set<String> openFiles = new HashSet<>();
