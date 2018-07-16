9fa57e794616c5a4f821ea4c2605651c160769cf
Markus <noreply@objectbox.io>
2018-07-16 17:31:40 +0200

    Setting BoxStore version to 2.0.0-2018-07-16

--- objectbox-java/src/main/java/io/objectbox/BoxStore.java
+++ objectbox-java/src/main/java/io/objectbox/BoxStore.java
@@ -59,7 +59,7 @@ import io.objectbox.reactive.SubscriptionBuilder;
 @ThreadSafe
 public class BoxStore implements Closeable {
 
-    private static final String VERSION = "2.0.0-2018-06-27";
+    private static final String VERSION = "2.0.0-2018-07-16";
     private static BoxStore defaultStore;
 
     /** Currently used DB dirs with values from {@link #getCanonicalPath(File)}. */
