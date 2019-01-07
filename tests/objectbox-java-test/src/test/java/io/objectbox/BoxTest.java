4928195358bac423b966a76306757e5d5d19ce8a
greenrobot Team <opensource+team@greenrobot>
2019-01-07 12:10:50 +0100

    Also test getAll2, log before operation.
    https://github.com/objectbox/objectbox-java/issues/626

--- tests/objectbox-java-test/src/test/java/io/objectbox/BoxTest.java
+++ tests/objectbox-java-test/src/test/java/io/objectbox/BoxTest.java
@@ -142,17 +142,45 @@ public class BoxTest extends AbstractObjectBoxTest {
         assertEquals(0, box.count());
         assertEquals(0, box.getAll().size());
 
+        System.out.println("PUT");
         List<TestEntity> entities = putTestEntities(10);
 
         // explicitly get an entity (any will do)
+        System.out.println("GET");
         TestEntity entity = box.get(entities.get(1).getId());
         assertNotNull(entity);
 
+        System.out.println("REMOVE_ALL");
         box.removeAll();
 
+        System.out.println("COUNT");
         assertEquals(0, box.count());
+        System.out.println("GET_ALL");
         List<TestEntity> all = box.getAll();
         // note only 1 entity is returned by getAll, it is the one we explicitly get (last) above
+        assertEquals(0, all.size());
+    }
+
+    // same as above, but using getAll2
+    @Test
+    public void testGetAll2AfterGetAndRemove() {
+        assertEquals(0, box.count());
+        assertEquals(0, box.getAll2().size());
+
+        System.out.println("PUT");
+        List<TestEntity> entities = putTestEntities(10);
+
+        System.out.println("GET");
+        TestEntity entity = box.get(entities.get(1).getId());
+        assertNotNull(entity);
+
+        System.out.println("REMOVE_ALL");
+        box.removeAll();
+
+        System.out.println("COUNT");
+        assertEquals(0, box.count());
+        System.out.println("GET_ALL2");
+        List<TestEntity> all = box.getAll2();
         assertEquals(0, all.size());
     }
 
