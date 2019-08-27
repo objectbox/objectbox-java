objectbox/objectbox-java

commit c598266efab6d537a4cb2b2570af97b69dfefa17

ToOne puts via cursor if necessary

objectbox-java/src/main/java/io/objectbox/relation/ToOne.java
@@ -6,7 +6,7 @@ import javax.annotation.Nullable;
 
 import io.objectbox.Box;
 import io.objectbox.BoxStore;
-import io.objectbox.Property;
+import io.objectbox.Cursor;
 import io.objectbox.annotation.apihint.Internal;
 import io.objectbox.exception.DbDetachedException;
 import io.objectbox.internal.ReflectionCache;
@@ -99,14 +99,14 @@ public class ToOne<TARGET> {
     }
 
     public void setTargetId(long targetId) {
-        if(relationInfo.targetIdProperty != null) {
+        if (relationInfo.targetIdProperty != null) {
             try {
                 getTargetIdField().set(entity, targetId);
             } catch (IllegalAccessException e) {
                 throw new RuntimeException("Could not update to-one ID in entity", e);
             }
         } else {
-            this.targetId= targetId;
+            this.targetId = targetId;
         }
     }
 
@@ -124,11 +124,7 @@ public class ToOne<TARGET> {
     // TODO provide a overload with a ToMany parameter, which also gets updated
     public void setTarget(@Nullable final TARGET target) {
         if (target != null) {
-            ensureBoxes(target);
-            long targetId = targetBox.getId(target);
-            if (targetId == 0) {
-                targetId = targetBox.put(target);
-            }
+            long targetId = relationInfo.targetInfo.getIdGetter().getId(target);
             setTargetId(targetId);
             setResolvedTarget(target, targetId);
         } else {
@@ -197,7 +193,7 @@ public class ToOne<TARGET> {
     }
 
     public long getTargetId() {
-        if(relationInfo.targetIdProperty != null) {
+        if (relationInfo.targetIdProperty != null) {
             // Future alternative: Implemented by generated ToOne sub classes to avoid reflection
             Field keyField = getTargetIdField();
             try {
@@ -218,8 +214,14 @@ public class ToOne<TARGET> {
         return targetIdField;
     }
 
-    @Internal
-    public void internalPrepareForPut(BoxStore boxStore) {
 
+    @Internal
+    public boolean internalRequiresPutTarget() {
+        return target != null && getTargetId() == 0;
+    }
+
+    @Internal
+    public void internalPutTarget(Cursor<TARGET> targetCursor) {
+        setTargetId(targetCursor.put(target));
     }
 }

objectbox-java/src/main/java/io/objectbox/Cursor.java
@@ -234,6 +234,13 @@ public abstract class Cursor<T> implements Closeable {
         return closed;
     }
 
+    protected <TARGET> Cursor<TARGET> getRelationTargetCursor(Class<TARGET> targetClass) {
+        // minor to do: optimize by using existing native cursor handle?
+        // (Note: Cursor should not destroy the native cursor then.)
+
+        return tx.createCursor(targetClass);
+    }
+
     public void renew(Transaction tx) {
         nativeRenew(cursor, tx.internalHandle());
         this.tx = tx;

objectbox-java/src/main/java/io/objectbox/relation/RelationInfo.java
@@ -9,6 +9,9 @@ import io.objectbox.annotation.apihint.Internal;
 
 @Internal
 @Immutable
+/**
+ * Meta info describing a relation including source and target entity.
+ */
 public class RelationInfo<TARGET> {
     public final EntityInfo sourceInfo;
     public final EntityInfo<TARGET> targetInfo;

tests/objectbox-java-test/src/main/java/io/objectbox/relation/CustomerCursor.java
@@ -5,6 +5,7 @@ import io.objectbox.BoxStore;
 import io.objectbox.Cursor;
 import io.objectbox.EntityInfo;
 import io.objectbox.Transaction;
+import io.objectbox.internal.CursorFactory;
 
 // THIS CODE IS ADAPTED from generated resources of the test-entity-annotations project
 
@@ -12,6 +13,12 @@ import io.objectbox.Transaction;
  * Cursor for DB entity "Customer".
  */
 public final class CustomerCursor extends Cursor<Customer> {
+
+    static final class Factory implements CursorFactory<Customer> {
+        public Cursor<Customer> createCursor(Transaction tx, long cursorHandle, BoxStore boxStoreForEntities) {
+            return new CustomerCursor(tx, cursorHandle, boxStoreForEntities);
+        }
+    }
 
     private static EntityInfo PROPERTIES = new Customer_();
 

tests/objectbox-java-test/src/main/java/io/objectbox/relation/Customer_.java
@@ -8,8 +8,10 @@ import io.objectbox.Cursor;
 import io.objectbox.EntityInfo;
 import io.objectbox.Property;
 import io.objectbox.Transaction;
+import io.objectbox.annotation.apihint.Internal;
 import io.objectbox.internal.CursorFactory;
 import io.objectbox.internal.IdGetter;
+import io.objectbox.relation.CustomerCursor.Factory;
 
 // THIS CODE IS ADAPTED from generated resources of the test-entity-annotations project
 
@@ -18,17 +20,45 @@ import io.objectbox.internal.IdGetter;
  */
 public class Customer_ implements EntityInfo<Customer> {
 
-    public static final String __NAME_IN_DB = "Customer";
+    // Leading underscores for static constants to avoid naming conflicts with property names
+
+    public static final String __ENTITY_NAME = "Customer";
+
+    public static final Class<Customer> __ENTITY_CLASS = Customer.class;
+
+    public static final String __DB_NAME = "Customer";
+
+    public static final CursorFactory<Customer> __CURSOR_FACTORY = new Factory();
+
+    @Internal
+    static final CustomerIdGetter __ID_GETTER = new CustomerIdGetter();
 
     public final static Property id = new Property(0, 1, long.class, "id", true, "_id");
     public final static Property name = new Property(1, 2, String.class, "name");
 
     public final static Property[] __ALL_PROPERTIES = {
-        id,
-        name
+            id,
+            name
     };
 
     public final static Property __ID_PROPERTY = id;
+
+    public final static Customer_ __INSTANCE = new Customer_();
+
+    @Override
+    public String getEntityName() {
+        return __ENTITY_NAME;
+    }
+
+    @Override
+    public Class<Customer> getEntityClass() {
+        return __ENTITY_CLASS;
+    }
+
+    @Override
+    public String getDbName() {
+        return __DB_NAME;
+    }
 
     @Override
     public Property[] getAllProperties() {
@@ -41,37 +71,23 @@ public class Customer_ implements EntityInfo<Customer> {
     }
 
     @Override
-    public String getEntityName() {
-        return "Customer";
-    }
-
-    @Override
-    public String getDbName() {
-        return __NAME_IN_DB;
-    }
-
-    @Override
-    public Class<Customer> getEntityClass() {
-        return Customer.class;
-    }
-
-    @Override
     public IdGetter<Customer> getIdGetter() {
-        return new IdGetter<Customer>() {
-            @Override
-            public long getId(Customer object) {
-                return object.getId();
-            }
-        };
+        return __ID_GETTER;
     }
 
     @Override
     public CursorFactory<Customer> getCursorFactory() {
-        return new CursorFactory<Customer>() {
-            @Override
-            public Cursor<Customer> createCursor(Transaction tx, long cursorHandle, @Nullable BoxStore boxStoreForEntities) {
-                return new CustomerCursor(tx, cursorHandle, boxStoreForEntities);
-            }
-        };
+        return __CURSOR_FACTORY;
     }
+
+    @Internal
+    static final class CustomerIdGetter implements IdGetter<Customer> {
+        public long getId(Customer object) {
+            return object.getId();
+        }
+    }
+
+    static final RelationInfo<Order> orders =
+            new RelationInfo<>(Customer_.__INSTANCE, Order_.__INSTANCE);
+
 }

tests/objectbox-java-test/src/main/java/io/objectbox/relation/Order.java
@@ -16,23 +16,21 @@ import io.objectbox.exception.DbDetachedException;
 public class Order {
 
     @Id(assignable = true)
-    private long id;
-    private java.util.Date date;
-    private long customerId;
-    private String text;
+    long id;
+    java.util.Date date;
+    long customerId;
+    String text;
 
     @Relation
     private Customer customer;
 
-
+    /** @Depreacted Used to resolve relations */
     @Internal
-    @Generated(hash = 1698848862)
-    private transient Long customer__resolvedKey;
-
-    /** Used to resolve relations */
-    @Internal
-    @Generated(hash = 1307364262)
+    @Generated(hash = 975972993)
     transient BoxStore __boxStore;
+    @Internal
+    @Generated(hash = 1031210392)
+    transient ToOne<Customer> customer__toOne = new ToOne<>(this, Order_.customer);
 
     @Generated(hash = 1105174599)
     public Order() {
@@ -89,30 +87,15 @@ public class Order {
     /** To-one relationship, resolved on first access. */
     @Generated(hash = 910495430)
     public Customer getCustomer() {
-        long __key = this.customerId;
-        if (customer__resolvedKey == null || customer__resolvedKey != __key) {
-            final BoxStore boxStore = this.__boxStore;
-            if (boxStore == null) {
-                throw new DbDetachedException();
-            }
-            Box<Customer> box = boxStore.boxFor(Customer.class);
-            Customer customerNew = box.get(__key);
-            synchronized (this) {
-                customer = customerNew;
-                customer__resolvedKey = __key;
-            }
-        }
+        customer = customer__toOne.getTarget(this.customerId);
         return customer;
     }
 
     /** Set the to-one relation including its ID property. */
     @Generated(hash = 1322376583)
     public void setCustomer(Customer customer) {
-        synchronized (this) {
-            this.customer = customer;
-            customerId = customer == null ? 0 : customer.getId();
-            customer__resolvedKey = customerId;
-        }
+        customer__toOne.setTarget(customer);
+        this.customer = customer;
     }
 
 }

tests/objectbox-java-test/src/main/java/io/objectbox/relation/OrderCursor.java
@@ -4,6 +4,7 @@ import io.objectbox.BoxStore;
 import io.objectbox.Cursor;
 import io.objectbox.EntityInfo;
 import io.objectbox.Transaction;
+import io.objectbox.internal.CursorFactory;
 
 // THIS CODE IS ADAPTED from generated resources of the test-entity-annotations project
 
@@ -11,8 +12,13 @@ import io.objectbox.Transaction;
  * Cursor for DB entity "ORDERS".
  */
 public final class OrderCursor extends Cursor<Order> {
+    static final class Factory implements CursorFactory<Order> {
+        public Cursor<Order> createCursor(Transaction tx, long cursorHandle, BoxStore boxStoreForEntities) {
+            return new OrderCursor(tx, cursorHandle, boxStoreForEntities);
+        }
+    }
 
-    private static EntityInfo PROPERTIES = new Order_();
+    private static final Order_.OrderIdGetter ID_GETTER = Order_.__ID_GETTER;
 
     // TODO private Query<Order> customer_OrdersQuery;
 
@@ -22,12 +28,12 @@ public final class OrderCursor extends Cursor<Order> {
     private final static int __ID_text = Order_.text.id;
 
     public OrderCursor(Transaction tx, long cursor, BoxStore boxStore) {
-        super(tx, cursor, PROPERTIES, boxStore);
+        super(tx, cursor, Order_.__INSTANCE, boxStore);
     }
 
     @Override
     public final long getId(Order entity) {
-        return entity.getId();
+        return ID_GETTER.getId(entity);
     }
 
     /**
@@ -37,15 +43,23 @@ public final class OrderCursor extends Cursor<Order> {
      */
     @Override
     public final long put(Order entity) {
+        if(entity.customer__toOne.internalRequiresPutTarget()) {
+            Cursor<Customer> targetCursor = getRelationTargetCursor(Customer.class);
+            try {
+                entity.customer__toOne.internalPutTarget(targetCursor);
+            } finally {
+                targetCursor.close();
+            }
+        }
         String text = entity.getText();
         int __id3 = text != null ? __ID_text : 0;
-        java.util.Date date = entity.getDate();
+        java.util.Date date = entity.date;
         int __id1 = date != null ? __ID_date : 0;
 
-        long __assignedId = collect313311(cursor, entity.getId(), PUT_FLAG_FIRST | PUT_FLAG_COMPLETE,
+        long __assignedId = collect313311(cursor, entity.id, PUT_FLAG_FIRST | PUT_FLAG_COMPLETE,
                 __id3, text, 0, null,
                 0, null, 0, null,
-                __ID_customerId, entity.getCustomerId(), __id1, __id1 != 0 ? date.getTime() : 0,
+                __ID_customerId, entity.customerId, __id1, __id1 != 0 ? date.getTime() : 0,
                 0, 0, 0, 0,
                 0, 0, 0, 0,
                 0, 0, 0, 0);
@@ -58,7 +72,7 @@ public final class OrderCursor extends Cursor<Order> {
     // TODO @Override
     protected final void attachEntity(Order entity) {
         // TODO super.attachEntity(entity);
-        entity.__boxStore = boxStoreForEntities;
+        //entity.__boxStore = boxStoreForEntities;
     }
 
     // TODO do we need this? @Override

tests/objectbox-java-test/src/main/java/io/objectbox/relation/Order_.java
@@ -8,8 +8,10 @@ import io.objectbox.Cursor;
 import io.objectbox.EntityInfo;
 import io.objectbox.Property;
 import io.objectbox.Transaction;
+import io.objectbox.annotation.apihint.Internal;
 import io.objectbox.internal.CursorFactory;
 import io.objectbox.internal.IdGetter;
+import io.objectbox.relation.OrderCursor.Factory;
 
 // THIS CODE IS ADAPTED from generated resources of the test-entity-annotations project
 
@@ -18,8 +20,19 @@ import io.objectbox.internal.IdGetter;
  */
 public class Order_ implements EntityInfo<Order> {
 
-    public static final String __NAME_IN_DB = "ORDERS";
-//    public static final String __NAME_IN_DB = "Order";
+
+    // Leading underscores for static constants to avoid naming conflicts with property names
+
+    public static final String __ENTITY_NAME = "Order";
+
+    public static final Class<Order> __ENTITY_CLASS = Order.class;
+
+    public static final String __DB_NAME = "ORDERS";
+
+    public static final CursorFactory<Order> __CURSOR_FACTORY = new Factory();
+
+    @Internal
+    static final OrderIdGetter __ID_GETTER = new OrderIdGetter();
 
     public final static Property id = new Property(0, 1, long.class, "id", true, "_id");
     public final static Property date = new Property(1, 2, java.util.Date.class, "date");
@@ -27,13 +40,30 @@ public class Order_ implements EntityInfo<Order> {
     public final static Property text = new Property(3, 4, String.class, "text");
 
     public final static Property[] __ALL_PROPERTIES = {
-        id,
-        date,
-        customerId,
-        text
+            id,
+            date,
+            customerId,
+            text
     };
 
     public final static Property __ID_PROPERTY = id;
+
+    public final static Order_ __INSTANCE = new Order_();
+
+    @Override
+    public String getEntityName() {
+        return __ENTITY_NAME;
+    }
+
+    @Override
+    public Class<Order> getEntityClass() {
+        return __ENTITY_CLASS;
+    }
+
+    @Override
+    public String getDbName() {
+        return __DB_NAME;
+    }
 
     @Override
     public Property[] getAllProperties() {
@@ -46,37 +76,22 @@ public class Order_ implements EntityInfo<Order> {
     }
 
     @Override
-    public String getEntityName() {
-        return "Order";
-    }
-
-    @Override
-    public String getDbName() {
-        return __NAME_IN_DB;
-    }
-
-    @Override
-    public Class<Order> getEntityClass() {
-        return Order.class;
-    }
-
-    @Override
-    public  IdGetter<Order> getIdGetter() {
-        return new IdGetter<Order>() {
-            @Override
-            public long getId(Order object) {
-                return object.getId();
-            }
-        };
+    public IdGetter<Order> getIdGetter() {
+        return __ID_GETTER;
     }
 
     @Override
     public CursorFactory<Order> getCursorFactory() {
-        return new CursorFactory<Order>() {
-            @Override
-            public Cursor<Order> createCursor(Transaction tx, long cursorHandle, @Nullable BoxStore boxStoreForEntities) {
-                return new OrderCursor(tx, cursorHandle, boxStoreForEntities);
-            }
-        };
+        return __CURSOR_FACTORY;
     }
+
+    @Internal
+    static final class OrderIdGetter implements IdGetter<Order> {
+        public long getId(Order object) {
+            return object.getId();
+        }
+    }
+
+    static final RelationInfo<Customer> customer = new RelationInfo<>(Order_.__INSTANCE, Customer_.__INSTANCE, customerId);
+
 }

commit b679463e2e039f16b65b2595ac56f509ac5ab8cd

ToOne docs: update setTarget() docs to match implementation.
Docs regression introduced with
c598266 ToOne puts via cursor if necessary

Also match wording (source entity vs enclosing entity, etc.).

objectbox-java/src/main/java/io/objectbox/relation/ToOne.java
@@ -148,6 +148,14 @@ public class ToOne<TARGET> implements Serializable {
         return getTargetId() == 0 && target == null;
     }
 
+    /**
+     * Sets or clears the target ID in the source entity. Pass 0 to clear.
+     * <p>
+     * Put the source entity to persist changes.
+     * If the ID is not 0 creates a relation to the target entity with this ID, otherwise dissolves it.
+     *
+     * @see #setTarget
+     */
     public void setTargetId(long targetId) {
         if (virtualProperty) {
             this.targetId = targetId;
@@ -172,10 +180,13 @@ public class ToOne<TARGET> implements Serializable {
     }
 
     /**
-     * Sets the relation ID in the enclosed entity to the ID of the given target entity.
-     * If the target entity was not put in the DB yet (its ID is 0), it will be put before to get its ID.
+     * Sets or clears the target entity and ID in the source entity. Pass null to clear.
+     * <p>
+     * Put the source entity to persist changes.
+     * If the target entity was not put yet (its ID is 0), it will be stored when the source entity is put.
+     *
+     * @see #setTargetId
      */
-    // TODO provide a overload with a ToMany parameter, which also gets updated
     public void setTarget(@Nullable final TARGET target) {
         if (target != null) {
             long targetId = relationInfo.targetInfo.getIdGetter().getId(target);
@@ -189,10 +200,11 @@ public class ToOne<TARGET> implements Serializable {
     }
 
     /**
-     * Sets the relation ID in the enclosed entity to the ID of the given target entity and puts the enclosed entity.
-     * If the target entity was not put in the DB yet (its ID is 0), it will be put before to get its ID.
+     * Sets or clears the target entity and ID in the source entity, then puts the source entity to persist changes.
+     * Pass null to clear.
+     * <p>
+     * If the target entity was not put yet (its ID is 0), it will be put before the source entity.
      */
-    // TODO provide a overload with a ToMany parameter, which also gets updated
     public void setAndPutTarget(@Nullable final TARGET target) {
         ensureBoxes(target);
         if (target != null) {
@@ -212,9 +224,13 @@ public class ToOne<TARGET> implements Serializable {
     }
 
     /**
-     * Sets the relation ID in the enclosed entity to the ID of the given target entity and puts both entities.
+     * Sets or clears the target entity and ID in the source entity,
+     * then puts the target (if not null) and source entity to persist changes.
+     * Pass null to clear.
+     * <p>
+     * When clearing the target entity, this does not remove it from its box.
+     * This only dissolves the relation.
      */
-    // TODO provide a overload with a ToMany parameter, which also gets updated
     public void setAndPutTargetAlways(@Nullable final TARGET target) {
         ensureBoxes(target);
         if (target != null) {