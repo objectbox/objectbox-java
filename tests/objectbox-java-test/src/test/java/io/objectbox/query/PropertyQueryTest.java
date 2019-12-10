cea1f1be7d76778cc0ef3e2b528c4259dca742b5
greenrobot Team <opensource+team@greenrobot>
2019-12-10 13:33:25 +0100

    PropertyQuery: test sumDouble with no data for integers.

--- tests/objectbox-java-test/src/test/java/io/objectbox/query/PropertyQueryTest.java
+++ tests/objectbox-java-test/src/test/java/io/objectbox/query/PropertyQueryTest.java
@@ -624,6 +624,16 @@ public class PropertyQueryTest extends AbstractQueryTest {
     @Test
     public void sumDouble_noData() {
         Query<TestEntity> baseQuery = box.query().build();
+        // Integer.
+        assertEquals(0, baseQuery.property(simpleByte).sumDouble(), 0.0001);
+        assertEquals(0, baseQuery.property(simpleInt).sumDouble(), 0.0001);
+        assertEquals(0, baseQuery.property(simpleShort).sumDouble(), 0.0001);
+        assertEquals(0, baseQuery.property(simpleLong).sumDouble(), 0.0001);
+        // Integer treated as unsigned.
+        assertEquals(0, baseQuery.property(simpleIntU).sumDouble(), 0.0001);
+        assertEquals(0, baseQuery.property(simpleShortU).sumDouble(), 0.0001);
+        assertEquals(0, baseQuery.property(simpleLongU).sumDouble(), 0.0001);
+        // Floating point.
         assertEquals(0, baseQuery.property(simpleFloat).sumDouble(), 0.0001);
         assertEquals(0, baseQuery.property(simpleDouble).sumDouble(), 0.0001);
     }
