a0a45022bdceeea243179e848d2a20a71c19bf1d
greenrobot Team <opensource+team@greenrobot>
2020-03-24 10:22:40 +0100

    Add Kotlin extensions to build Rx types, requires Java 8 for Kotlin library.

---
+++ objectbox-kotlin/src/main/kotlin/io/objectbox/kotlin/Query.kt
@@ -0,0 +1,29 @@
+package io.objectbox.kotlin
+
+import io.objectbox.query.Query
+import io.objectbox.rx3.RxQuery
+import io.reactivex.rxjava3.core.BackpressureStrategy
+import io.reactivex.rxjava3.core.Flowable
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
+
+/**
+ * Shortcut for [`RxQuery.flowableOneByOne(query, strategy)`][RxQuery.flowableOneByOne].
+ */
+fun <T> Query<T>.flowableOneByOne(strategy: BackpressureStrategy = BackpressureStrategy.BUFFER): Flowable<T> {
+    return RxQuery.flowableOneByOne(this, strategy)
+}
+
+/**
+ * Shortcut for [`RxQuery.observable(query)`][RxQuery.observable].
+ */
+fun <T> Query<T>.observable(): Observable<MutableList<T>> {
+    return RxQuery.observable(this)
+}
+
+/**
+ * Shortcut for [`RxQuery.single(query)`][RxQuery.single].
+ */
+fun <T> Query<T>.single(): Single<MutableList<T>> {
+    return RxQuery.single(this)
+}
