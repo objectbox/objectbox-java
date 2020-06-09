47a05a7096f4139bc8cda2d6999a78dff77606df
greenrobot Team <opensource+team@greenrobot>
2020-06-09 12:11:43 +0200

    Prepare version 2.6.0.

--- objectbox-java/src/main/java/io/objectbox/BoxStore.java
+++ objectbox-java/src/main/java/io/objectbox/BoxStore.java
@@ -64,9 +64,9 @@ public class BoxStore implements Closeable {
     @Nullable private static Object relinker;
 
     /** Change so ReLinker will update native library when using workaround loading. */
-    public static final String JNI_VERSION = "2.6.0-RC";
+    public static final String JNI_VERSION = "2.6.0";
 
-    private static final String VERSION = "2.6.0-2020-04-30";
+    private static final String VERSION = "2.6.0-2020-06-09";
     private static BoxStore defaultStore;
 
     /** Currently used DB dirs with values from {@link #getCanonicalPath(File)}. */
