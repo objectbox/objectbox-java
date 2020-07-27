5b2a0c8a685ad8e89d5ed0e629be9531de8be9d9
greenrobot Team <opensource+team@greenrobot>
2020-07-27 15:02:34 +0200

    FIXME Add FileCorruptException.

---
+++ objectbox-java/src/main/java/io/objectbox/exception/FileCorruptException.java
@@ -0,0 +1,8 @@
+package io.objectbox.exception;
+
+// TODO Docs, more constructors?
+public class FileCorruptException extends DbException {
+    public FileCorruptException(String message) {
+        super(message);
+    }
+}
