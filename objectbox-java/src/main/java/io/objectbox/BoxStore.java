67d778345fd561d132ed97b6654786f47ec877ce
Markus <markus@greenrobot>
2020-08-19 15:07:39 +0200

    VERSION = "2.7.1-2020-08-19"

--- objectbox-java/src/main/java/io/objectbox/BoxStore.java
+++ objectbox-java/src/main/java/io/objectbox/BoxStore.java
@@ -65,9 +65,9 @@ public class BoxStore implements Closeable {
     @Nullable private static Object relinker;
 
     /** Change so ReLinker will update native library when using workaround loading. */
-    public static final String JNI_VERSION = "2.7.0";
+    public static final String JNI_VERSION = "2.7.1";
 
-    private static final String VERSION = "2.7.0-2020-07-30";
+    private static final String VERSION = "2.7.1-2020-08-19";
     private static BoxStore defaultStore;
 
     /** Currently used DB dirs with values from {@link #getCanonicalPath(File)}. */
