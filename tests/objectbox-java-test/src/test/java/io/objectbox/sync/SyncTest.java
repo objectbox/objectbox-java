23b1dbee1415daf5c93cef6096430bbccc835279
greenrobot Team <opensource+team@greenrobot>
2020-09-01 15:27:20 +0200

    Test creating Sync client throws.

--- tests/objectbox-java-test/src/test/java/io/objectbox/sync/SyncTest.java
+++ tests/objectbox-java-test/src/test/java/io/objectbox/sync/SyncTest.java
@@ -2,13 +2,15 @@ package io.objectbox.sync;
 
 import org.junit.Test;
 
-
+import io.objectbox.AbstractObjectBoxTest;
 import io.objectbox.BoxStore;
 
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThrows;
 
-public class SyncTest {
+public class SyncTest extends AbstractObjectBoxTest {
 
     /**
      * Ensure that non-sync native library correctly reports sync client availability.
@@ -28,5 +30,14 @@ public class SyncTest {
     @Test
     public void serverIsNotAvailable() {
         assertFalse(BoxStore.isSyncServerAvailable());
+    }
+
+    @Test
+    public void creatingSyncClient_throws() {
+        IllegalStateException exception = assertThrows(
+                IllegalStateException.class,
+                () -> Sync.client(store, "wss://127.0.0.1", SyncCredentials.none())
+        );
+        assertEquals("This ObjectBox library (JNI) does not include sync. Please update your dependencies.", exception.getMessage());
     }
 }
