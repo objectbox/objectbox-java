objectbox/objectbox-java

commit e108fd00beb42e072a4c78c49a45e815f4d5f775

BoxStore: make setSyncClient() package private

objectbox-java/src/main/java/io/objectbox/InternalAccess.java
@@ -17,6 +17,7 @@
 package io.objectbox;
 
 import io.objectbox.annotation.apihint.Internal;
+import io.objectbox.sync.SyncClient;
 
 @Internal
 public class InternalAccess {
@@ -34,6 +35,10 @@ public class InternalAccess {
 
     public static long getHandle(Transaction tx) {
         return tx.internalHandle();
+    }
+
+    public static void setSyncClient(BoxStore boxStore, SyncClient syncClient) {
+        boxStore.setSyncClient(syncClient);
     }
 
     public static <T> void releaseReader(Box<T> box, Cursor<T> reader) {

objectbox-java/src/main/java/io/objectbox/BoxStore.java
@@ -1178,7 +1178,7 @@ public class BoxStore implements Closeable {
         return syncClient;
     }
 
-    public void setSyncClient(@Nullable SyncClient syncClient) {
+    void setSyncClient(@Nullable SyncClient syncClient) {
         this.syncClient = syncClient;
     }
 }

objectbox-java/src/main/java/io/objectbox/sync/SyncClientImpl.java
@@ -63,7 +63,7 @@ public class SyncClientImpl implements SyncClient {
         setLoginCredentials(builder.credentials);
 
         // If created successfully, let store keep a reference so the caller does not have to.
-        builder.boxStore.setSyncClient(this);
+        InternalAccess.setSyncClient(builder.boxStore, this);
 
         if (!builder.manualStart) {
             start();
@@ -169,7 +169,7 @@ public class SyncClientImpl implements SyncClient {
             if (boxStore != null) {
                 SyncClient syncClient = boxStore.getSyncClient();
                 if (syncClient == this) {
-                    boxStore.setSyncClient(null);
+                    InternalAccess.setSyncClient(boxStore, null);
                 }
                 this.boxStore = null;
             }

commit 2235b1639c0966a1b51319b8e8adc461ffde0c02

Spotbugs: add missing nullable annotation.
Regression from
e108fd00 BoxStore: make setSyncClient() package private

objectbox-java/src/main/java/io/objectbox/InternalAccess.java
@@ -16,6 +16,8 @@
 
 package io.objectbox;
 
+import javax.annotation.Nullable;
+
 import io.objectbox.annotation.apihint.Internal;
 import io.objectbox.sync.SyncClient;
 
@@ -37,7 +39,7 @@ public class InternalAccess {
         return tx.internalHandle();
     }
 
-    public static void setSyncClient(BoxStore boxStore, SyncClient syncClient) {
+    public static void setSyncClient(BoxStore boxStore, @Nullable SyncClient syncClient) {
         boxStore.setSyncClient(syncClient);
     }
 