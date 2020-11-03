bc9a33a27861b182177a070dcf44929c13f25251
greenrobot Team <13865709+greenrobot-team@users.noreply.github.com>
2020-11-03 09:56:41 +0100

    Sync: add missing SyncLoginCodes.

--- objectbox-java/src/main/java/io/objectbox/sync/SyncLoginCodes.java
+++ objectbox-java/src/main/java/io/objectbox/sync/SyncLoginCodes.java
@@ -10,11 +10,13 @@ import io.objectbox.sync.listener.SyncListener;
 public class SyncLoginCodes {
 
     public static final long OK = 20;
+    public static final long REQ_REJECTED = 40;
     public static final long CREDENTIALS_REJECTED = 43;
     public static final long UNKNOWN = 50;
     public static final long AUTH_UNREACHABLE = 53;
     public static final long BAD_VERSION = 55;
     public static final long CLIENT_ID_TAKEN = 61;
+    public static final long TX_VIOLATED_UNIQUE = 71;
 
     private SyncLoginCodes() {
     }
