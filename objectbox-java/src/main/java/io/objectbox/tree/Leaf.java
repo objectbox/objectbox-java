ea66aa93a0d75bed9a559140a20eee228be55868
greenrobot Team <13865709+greenrobot-team@users.noreply.github.com>
2021-05-04 14:38:50 +0200

    Tree API: prepare leaf value getters.

--- objectbox-java/src/main/java/io/objectbox/tree/Leaf.java
+++ objectbox-java/src/main/java/io/objectbox/tree/Leaf.java
@@ -39,26 +39,103 @@ public class Leaf {
 
     // valueInt
     @Nullable
-    public Long asInt() {
-        throw new UnsupportedOperationException();
+    public Long getInt() {
+        if (!isInt()) throw new IllegalStateException("value is not integer");
+        return node.integerValue;
     }
 
     // valueDouble
     @Nullable
-    public Double asDouble() {
-        throw new UnsupportedOperationException();
+    public Double getDouble() {
+        if (!isDouble()) throw new IllegalStateException("value is not floating point");
+        return node.floatingValue;
     }
 
     // valueString
     @Nullable
-    public String asString() {
-        throw new UnsupportedOperationException();
+    public String getString() {
+        if (!isString()) throw new IllegalStateException("value is not string");
+        return (String) node.objectValue;
     }
 
     // valueStrings
     @Nullable
+    public String[] getStringArray() {
+        if (!isStringArray()) throw new IllegalStateException("value is not string array");
+        return (String[]) node.objectValue;
+    }
+
+    @Nullable
+    public Long asInt() {
+        if (isInt()) return getInt();
+
+        if (isDouble()) {
+            Double value = getDouble();
+            return value != null ? value.longValue() : null;
+        }
+        if (isString()) {
+            String value = getString();
+            return value != null ? Long.valueOf(value) : null;
+        }
+
+        return null;
+    }
+
+    @Nullable
+    public Double asDouble() {
+        if (isDouble()) return getDouble();
+
+        if (isInt()) {
+            Long value = getInt();
+            return value != null ? value.doubleValue() : null;
+        }
+        if (isString()) {
+            String value = getString();
+            return value != null ? Double.valueOf(value) : null;
+        }
+
+        return null;
+    }
+
+    @Nullable
+    public String asString() {
+        if (isString()) return getString();
+
+        if (isInt()) {
+            Long value = getInt();
+            return value != null ? String.valueOf(value) : null;
+        }
+        if (isDouble()) {
+            Double value = getDouble();
+            return value != null ? String.valueOf(value) : null;
+        }
+        if (isStringArray()) {
+            // Return first item.
+            String[] value = getStringArray();
+            return value != null && value.length > 0 ? value[0] : null;
+        }
+
+        return null;
+    }
+
+    @Nullable
     public String[] asStringArray() {
-        throw new UnsupportedOperationException();
+        if (isStringArray()) return getStringArray();
+
+        if (isInt()) {
+            Long value = getInt();
+            return value != null ? new String[]{String.valueOf(value)} : null;
+        }
+        if (isDouble()) {
+            Double value = getDouble();
+            return value != null ? new String[]{String.valueOf(value)} : null;
+        }
+        if (isString()) {
+            String value = getString();
+            return value != null ? new String[]{value} : null;
+        }
+
+        return null;
     }
 
     public void setInt(@Nullable Long value) {
