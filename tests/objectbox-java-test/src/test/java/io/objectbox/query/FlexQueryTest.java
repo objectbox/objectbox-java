f103488facbc7c2a36493b6bdfe1397c9876f600
greenrobot Team <13865709+greenrobot-team@users.noreply.github.com>
2021-12-14 10:40:04 +0100

    Test equals and containsElement for flex property.

--- tests/objectbox-java-test/src/test/java/io/objectbox/query/FlexQueryTest.java
+++ tests/objectbox-java-test/src/test/java/io/objectbox/query/FlexQueryTest.java
@@ -4,16 +4,88 @@ import io.objectbox.TestEntity;
 import io.objectbox.TestEntity_;
 import org.junit.Test;
 
+import javax.annotation.Nullable;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
 public class FlexQueryTest extends AbstractQueryTest {
+
+    private TestEntity createFlexPropertyEntity(@Nullable Object flex) {
+        TestEntity entity = new TestEntity();
+        entity.setFlexProperty(flex);
+        return entity;
+    }
+
+    /**
+     * equals works for flexible string and integer properties.
+     */
+    @Test
+    public void equals_flexString() {
+        box.put(
+                createFlexPropertyEntity("banana"),
+                createFlexPropertyEntity(-1),
+                createFlexPropertyEntity(1.3f),
+                createFlexPropertyEntity(-1.4d),
+                createFlexPropertyEntity(null)
+        );
+
+        assertFlexPropertyEqualsMatch("banana");
+        assertFlexPropertyEqualsMatch(-1);
+
+        // Check isNull as well
+        List<TestEntity> results = box.query(TestEntity_.flexProperty.isNull()).build().find();
+        assertEquals(1, results.size());
+        assertNull(results.get(0).getFlexProperty());
+    }
+
+    private void assertFlexPropertyEqualsMatch(Object value) {
+        List<TestEntity> results = box.query(TestEntity_.flexProperty.equal(value.toString())).build().find();
+        assertEquals(1, results.size());
+        assertEquals(value, results.get(0).getFlexProperty());
+    }
+
+    /**
+     * containsElement matches strings and integers of flexible list.
+     */
+    @Test
+    public void containsElement_flexList() {
+        List<Object> flexListMatch = new ArrayList<>();
+        flexListMatch.add("banana");
+        flexListMatch.add(12);
+        List<Object> flexListNoMatch = new ArrayList<>();
+        flexListNoMatch.add("banana milk shake");
+        flexListNoMatch.add(1234);
+        List<Object> flexListNotSupported = new ArrayList<>();
+        flexListNotSupported.add(1.3f);
+        flexListNotSupported.add(-1.4d);
+        box.put(
+                createFlexPropertyEntity(flexListMatch),
+                createFlexPropertyEntity(flexListNoMatch),
+                createFlexPropertyEntity(flexListNotSupported),
+                createFlexPropertyEntity(null)
+        );
+
+        assertFlexListContainsElement("banana");
+        assertFlexListContainsElement(12);
+    }
+
+    @SuppressWarnings("unchecked")
+    private void assertFlexListContainsElement(Object value) {
+        List<TestEntity> results = box.query(TestEntity_.flexProperty.containsElement(value.toString())).build().find();
+        assertEquals(1, results.size());
+        List<Object> list = (List<Object>) results.get(0).getFlexProperty();
+        assertNotNull(list);
+        assertTrue(list.contains("banana"));
+    }
 
     private TestEntity createFlexMapEntity(String s, boolean b, long l, float f, double d) {
         TestEntity entity = new TestEntity();
@@ -63,7 +135,7 @@ public class FlexQueryTest extends AbstractQueryTest {
                 TestEntity_.stringObjectMap.containsKeyValue("", "").alias("contains")
         ).build();
         assertEquals(0, setParamQuery.find().size());
-        
+
         setParamQuery.setParameters(TestEntity_.stringObjectMap, "banana-string", "banana");
         List<TestEntity> setParamResults = setParamQuery.find();
         assertEquals(1, setParamResults.size());
