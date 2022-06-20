59341061612b74458eaadc2ccf0607e87edd876b
Uwe <13865709+greenrobot-team@users.noreply.github.com>
2022-06-20 15:36:13 +0200

    BoxStore: set version to 3.2.0-2022-06-15.

--- objectbox-java/src/main/java/io/objectbox/BoxStore.java
+++ objectbox-java/src/main/java/io/objectbox/BoxStore.java
@@ -71,7 +71,7 @@ public class BoxStore implements Closeable {
     /** Change so ReLinker will update native library when using workaround loading. */
     public static final String JNI_VERSION = "3.1.3";
 
-    private static final String VERSION = "3.1.3-2022-05-06";
+    private static final String VERSION = "3.2.0-2022-06-15";
     private static BoxStore defaultStore;
 
     /** Currently used DB dirs with values from {@link #getCanonicalPath(File)}. */
