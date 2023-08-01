1afdf6b84b0968f6a4c2d3f5b49060e4e7741b96
Uwe <13865709+greenrobot-team@users.noreply.github.com>
2023-08-01 13:29:46 +0200

    Gradle: convert settings.gradle to KTS.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,8 @@
-include ':objectbox-java-api'
-include ':objectbox-java'
-include ':objectbox-kotlin'
-include ':objectbox-rxjava'
-include ':objectbox-rxjava3'
+include(":objectbox-java-api")
+include(":objectbox-java")
+include(":objectbox-kotlin")
+include(":objectbox-rxjava")
+include(":objectbox-rxjava3")
 
-include ':tests:objectbox-java-test'
-include ':tests:test-proguard'
+include(":tests:objectbox-java-test")
+include(":tests:test-proguard")
