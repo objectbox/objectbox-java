objectbox/objectbox-java

commit c9ff41fe0719175271fd693d81301c7f0722bce6

In-memory: use native implementation for deleteAllFiles #194
The native implementation is capable of cleaning up in-memory databases
as well.

objectbox-java/src/main/java/io/objectbox/BoxStore.java
@@ -16,6 +16,8 @@
 
 package io.objectbox;
 
+import org.greenrobot.essentials.collections.LongHashMap;
+
 import java.io.Closeable;
 import java.io.File;
 import java.io.IOException;
@@ -55,7 +57,6 @@ import io.objectbox.reactive.DataObserver;
 import io.objectbox.reactive.DataPublisher;
 import io.objectbox.reactive.SubscriptionBuilder;
 import io.objectbox.sync.SyncClient;
-import org.greenrobot.essentials.collections.LongHashMap;
 
 /**
  * An ObjectBox database that provides {@link Box Boxes} to put and get objects of specific entity classes
@@ -139,6 +140,12 @@ public class BoxStore implements Closeable {
         NativeLibraryLoader.ensureLoaded();
         return nativeGetVersion();
     }
+
+    /**
+     * @return true if DB files did not exist or were successfully removed,
+     * false if DB files exist that could not be removed.
+     */
+    static native boolean nativeRemoveDbFiles(String directory, boolean removeDir);
 
     /**
      * Creates a native BoxStore instance with FlatBuffer {@link FlatStoreOptions} {@code options}
@@ -690,38 +697,30 @@ public class BoxStore implements Closeable {
     /**
      * Danger zone! This will delete all files in the given directory!
      * <p>
-     * No {@link BoxStore} may be alive using the given directory.
+     * No {@link BoxStore} may be alive using the given directory. E.g. call this before building a store. When calling
+     * this after {@link #close() closing} a store, read the docs of that method carefully first!
      * <p>
-     * If you did not use a custom name with BoxStoreBuilder, you can pass "new File({@link
-     * BoxStoreBuilder#DEFAULT_NAME})".
+     * If no {@link BoxStoreBuilder#name(String) name} was specified when building the store, use like:
+     *
+     * <pre>{@code
+     *     BoxStore.deleteAllFiles(new File(BoxStoreBuilder.DEFAULT_NAME));
+     * }</pre>
+     *
+     * <p>For an {@link BoxStoreBuilder#inMemory(String) in-memory} database, this will just clean up the in-memory
+     * database.
      *
      * @param objectStoreDirectory directory to be deleted; this is the value you previously provided to {@link
      * BoxStoreBuilder#directory(File)}
      * @return true if the directory 1) was deleted successfully OR 2) did not exist in the first place.
      * Note: If false is returned, any number of files may have been deleted before the failure happened.
-     * @throws IllegalStateException if the given directory is still used by a open {@link BoxStore}.
+     * @throws IllegalStateException if the given directory is still used by an open {@link BoxStore}.
      */
     public static boolean deleteAllFiles(File objectStoreDirectory) {
-        if (!objectStoreDirectory.exists()) {
-            return true;
-        }
-        if (isFileOpen(getCanonicalPath(objectStoreDirectory))) {
+        String canonicalPath = getCanonicalPath(objectStoreDirectory);
+        if (isFileOpen(canonicalPath)) {
             throw new IllegalStateException("Cannot delete files: store is still open");
         }
-
-        File[] files = objectStoreDirectory.listFiles();
-        if (files == null) {
-            return false;
-        }
-        for (File file : files) {
-            if (!file.delete()) {
-                // OK if concurrently deleted. Fail fast otherwise.
-                if (file.exists()) {
-                    return false;
-                }
-            }
-        }
-        return objectStoreDirectory.delete();
+        return nativeRemoveDbFiles(canonicalPath, true);
     }
 
     /**

tests/objectbox-java-test/src/test/java/io/objectbox/AbstractObjectBoxTest.java
@@ -117,8 +117,7 @@ public abstract class AbstractObjectBoxTest {
         if (IN_MEMORY) {
             // Instead of random temp directory, use random suffix for each test to avoid re-using existing database
             // from other tests in case clean-up fails.
-            // Note: all clean-up code will gracefully fail (e.g. deleting the database files will do nothing as the
-            // directory does not exist).
+            // Note: tearDown code will still work as the directory does not exist.
             String randomPart = Long.toUnsignedString(random.nextLong());
             return new File(BoxStore.IN_MEMORY_PREFIX + prefix + randomPart);
         } else {
@@ -178,10 +177,13 @@ public abstract class AbstractObjectBoxTest {
                 logError("Could not clean up test", e);
             }
         }
-        deleteAllFiles(boxStoreDir);
+        cleanUpAllFiles(boxStoreDir);
     }
 
-    protected void deleteAllFiles(@Nullable File boxStoreDir) {
+    /**
+     * Manually clean up any leftover files to prevent interference with other tests.
+     */
+    protected void cleanUpAllFiles(@Nullable File boxStoreDir) {
         if (boxStoreDir != null && boxStoreDir.exists()) {
             try (Stream<Path> stream = Files.walk(boxStoreDir.toPath())) {
                 stream.sorted(Comparator.reverseOrder())

tests/objectbox-java-test/src/test/java/io/objectbox/BoxStoreBuilderTest.java
@@ -120,7 +120,7 @@ public class BoxStoreBuilderTest extends AbstractObjectBoxTest {
             }
         }
 
-        deleteAllFiles(parentTestDir);
+        cleanUpAllFiles(parentTestDir);
     }
 
     @Test

tests/objectbox-java-test/src/test/java/io/objectbox/BoxStoreTest.java
@@ -181,7 +181,8 @@ public class BoxStoreTest extends AbstractObjectBoxTest {
 
     @Test
     public void testDeleteAllFiles() {
-        assumeFalse(IN_MEMORY);
+        // Note: for in-memory can not really assert database is gone,
+        // relying on native code returning true for deleteAllFiles.
         closeStoreForTest();
     }
 
@@ -228,7 +229,6 @@ public class BoxStoreTest extends AbstractObjectBoxTest {
 
     @Test(expected = IllegalStateException.class)
     public void testDeleteAllFiles_openStore() {
-        assumeFalse(IN_MEMORY);
         BoxStore.deleteAllFiles(boxStoreDir);
     }
 
@@ -258,9 +258,7 @@ public class BoxStoreTest extends AbstractObjectBoxTest {
             assertTrue(boxStoreDir.exists());
         }
         store.close();
-        if (!IN_MEMORY) {
-            assertTrue(store.deleteAllFiles());
-        }
+        assertTrue(store.deleteAllFiles());
         assertFalse(boxStoreDir.exists());
     }
 

tests/objectbox-java-test/src/test/java/io/objectbox/BoxStoreValidationTest.java
@@ -87,7 +87,7 @@ public class BoxStoreValidationTest extends AbstractObjectBoxTest {
         assertEquals("Validating pages failed (page not found)", ex.getMessage());
 
         // Clean up
-        deleteAllFiles(dir);
+        cleanUpAllFiles(dir);
     }
 
     @Test
@@ -160,7 +160,7 @@ public class BoxStoreValidationTest extends AbstractObjectBoxTest {
                 ex.getMessage());
 
         // Clean up
-        deleteAllFiles(dir);
+        cleanUpAllFiles(dir);
     }
 
     /**

commit ea6af86ad1dcab0b19edc53fda5eaec62b9b8390

Regression: ensure native library is loaded in static methods as needed.
Regression from:
c9ff41fe In-memory: use native implementation for deleteAllFiles #194

Notably when calling the static deleteAllFiles which now uses a native
implementation.

objectbox-java/src/main/java/io/objectbox/BoxStore.java
@@ -453,6 +453,7 @@ public class BoxStore implements Closeable {
      */
     @Experimental
     public static long sysProcMeminfoKb(String key) {
+        NativeLibraryLoader.ensureLoaded();
         return nativeSysProcMeminfoKb(key);
     }
 
@@ -475,6 +476,7 @@ public class BoxStore implements Closeable {
      */
     @Experimental
     public static long sysProcStatusKb(String key) {
+        NativeLibraryLoader.ensureLoaded();
         return nativeSysProcStatusKb(key);
     }
 
@@ -720,6 +722,7 @@ public class BoxStore implements Closeable {
         if (isFileOpen(canonicalPath)) {
             throw new IllegalStateException("Cannot delete files: store is still open");
         }
+        NativeLibraryLoader.ensureLoaded();
         return nativeRemoveDbFiles(canonicalPath, true);
     }
 