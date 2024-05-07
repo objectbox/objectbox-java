9e9ae5c06ae7613e662d5111744a135b789cb468
Uwe <13865709+greenrobot-team@users.noreply.github.com>
2024-05-07 11:49:50 +0200

    HNSW index: add new distance types

--- objectbox-java-api/src/main/java/io/objectbox/annotation/HnswDistanceType.java
+++ objectbox-java-api/src/main/java/io/objectbox/annotation/HnswDistanceType.java
@@ -29,5 +29,35 @@ public enum HnswDistanceType {
     /**
      * Typically "Euclidean squared" internally.
      */
-    EUCLIDEAN
+    EUCLIDEAN,
+
+    /**
+     * Cosine similarity compares two vectors irrespective of their magnitude (compares the angle of two vectors).
+     * <p>
+     * Often used for document or semantic similarity.
+     * <p>
+     * Value range: 0.0 - 2.0 (0.0: same direction, 1.0: orthogonal, 2.0: opposite direction)
+     */
+    COSINE,
+
+    /**
+     * For normalized vectors (vector length == 1.0), the dot product is equivalent to the cosine similarity.
+     * <p>
+     * Because of this, the dot product is often preferred as it performs better.
+     * <p>
+     * Value range (normalized vectors): 0.0 - 2.0 (0.0: same direction, 1.0: orthogonal, 2.0: opposite direction)
+     */
+    DOT_PRODUCT,
+
+    /**
+     * A custom dot product similarity measure that does not require the vectors to be normalized.
+     * <p>
+     * Note: this is no replacement for cosine similarity (like DotProduct for normalized vectors is). The non-linear
+     * conversion provides a high precision over the entire float range (for the raw dot product). The higher the dot
+     * product, the lower the distance is (the nearer the vectors are). The more negative the dot product, the higher
+     * the distance is (the farther the vectors are).
+     * <p>
+     * Value range: 0.0 - 2.0 (nonlinear; 0.0: nearest, 1.0: orthogonal, 2.0: farthest)
+     */
+    DOT_PRODUCT_NON_NORMALIZED
 }
