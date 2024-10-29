53615b51f944ee1e3434e491922e2236b277c239
Uwe <noreply+uwe@objectbox.io>
2024-10-29 15:31:18 +0100

    Add BoxStoreLogTest, test only pool threads are printed

---
+++ tests/objectbox-java-test/src/test/java/io/objectbox/BoxStoreLogTest.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2024 ObjectBox Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.objectbox;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+
+import javax.annotation.Nullable;
+
+import io.objectbox.annotation.IndexType;
+
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Sets a custom error output stream to assert log messages of {@link BoxStore}.
+ */
+public class BoxStoreLogTest extends AbstractObjectBoxTest {
+
+    private ByteArrayOutputStream errOutput;
+
+    @Override
+    protected BoxStoreBuilder createBoxStoreBuilder(@Nullable IndexType simpleStringIndexType) {
+        BoxStoreBuilder builder = super.createBoxStoreBuilder(simpleStringIndexType);
+        errOutput = new ByteArrayOutputStream();
+        builder.setErrorOutput(new PrintStream(errOutput));
+        return builder;
+    }
+
+    @Test
+    public void close_activeThreadPool_printsError() throws UnsupportedEncodingException {
+        // Submit two threads, one to the internal pool, that run longer
+        // than BoxStore.close waits on the pool to terminate
+        new Thread(() -> {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException ignored) {
+            }
+        }).start();
+        store.internalScheduleThread(() -> {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException ignored) {
+            }
+        });
+        // Close store to trigger thread pool shutdown, store waits 1 second for shutdown
+        store.close();
+
+        String errOutput = this.errOutput.toString("UTF-8");
+        assertTrue(errOutput.contains("ObjectBox thread pool not terminated in time"));
+        assertTrue(errOutput.contains("Printing pool threads..."));
+        assertTrue(errOutput.contains("Printing pool threads...DONE"));
+        // Check that only ObjectBox threads are printed
+        String[] lines = errOutput.split("\n");
+        for (String line : lines) {
+            if (line.startsWith("Thread:")) {
+                assertTrue("Expected thread name to start with 'ObjectBox-' but was: " + line, line.contains("ObjectBox-"));
+            }
+        }
+    }
+
+}
