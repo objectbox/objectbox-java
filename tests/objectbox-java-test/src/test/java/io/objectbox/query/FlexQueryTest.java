2f984c5230ace5d50aca672170be4fc20ece286e
Uwe <noreply+uwe@objectbox.io>
2025-01-28 07:09:07 +0100

    Tests: ignore broken contains_stringObjectMap due to API changes objectbox#1099
    The native query API for flex map properties has changed and expects
    integers to no longer be passed as strings.

--- tests/objectbox-java-test/src/test/java/io/objectbox/query/FlexQueryTest.java
+++ tests/objectbox-java-test/src/test/java/io/objectbox/query/FlexQueryTest.java
@@ -1,15 +1,35 @@
+/*
+ * Copyright 2025 ObjectBox Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.objectbox.query;
 
-import io.objectbox.TestEntity;
-import io.objectbox.TestEntity_;
+import org.junit.Ignore;
 import org.junit.Test;
 
-import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.annotation.Nullable;
+
+import io.objectbox.TestEntity;
+import io.objectbox.TestEntity_;
+
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -103,6 +123,7 @@ public class FlexQueryTest extends AbstractQueryTest {
         return entity;
     }
 
+    @Ignore("Broken due to flex map query API changes, see objectbox#1099")
     @Test
     public void contains_stringObjectMap() {
         // Note: map keys and values can not be null, so no need to test. See FlexMapConverterTest.
