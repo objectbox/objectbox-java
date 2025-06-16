69b16de9ea08ece0936f20843cce42a41a5e9d50
Uwe <noreply+uwe@objectbox.io>
2025-06-16 12:31:50 +0200

    Gradle KTS: convert objectbox-rxjava and -jxjava3 build scripts

--- objectbox-rxjava/build.gradle.kts
+++ objectbox-rxjava/build.gradle.kts
@@ -5,38 +5,43 @@ plugins {
 
 // Note: use release flag instead of sourceCompatibility and targetCompatibility to ensure only JDK 8 API is used.
 // https://docs.gradle.org/current/userguide/building_java_projects.html#sec:java_cross_compilation
-tasks.withType(JavaCompile).configureEach {
+tasks.withType<JavaCompile> {
     options.release.set(8)
 }
 
+val junitVersion: String by rootProject.extra
+val mockitoVersion: String by rootProject.extra
+
 dependencies {
-    api project(':objectbox-java')
-    api 'io.reactivex.rxjava2:rxjava:2.2.21'
+    api(project(":objectbox-java"))
+    api("io.reactivex.rxjava2:rxjava:2.2.21")
 
-    testImplementation "junit:junit:$junitVersion"
-    testImplementation "org.mockito:mockito-core:$mockitoVersion"
+    testImplementation("junit:junit:$junitVersion")
+    testImplementation("org.mockito:mockito-core:$mockitoVersion")
 }
 
-tasks.register('javadocJar', Jar) {
-    dependsOn javadoc
-    archiveClassifier.set('javadoc')
-    from 'build/docs/javadoc'
+val javadocJar by tasks.registering(Jar::class) {
+    dependsOn(tasks.named("javadoc"))
+    archiveClassifier.set("javadoc")
+    from("build/docs/javadoc")
 }
 
-tasks.register('sourcesJar', Jar) {
-    archiveClassifier.set('sources')
-    from sourceSets.main.allSource
+val sourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
 }
 
 // Set project-specific properties.
-publishing.publications {
-    mavenJava(MavenPublication) {
-        from components.java
-        artifact sourcesJar
-        artifact javadocJar
-        pom {
-            name = 'ObjectBox RxJava API'
-            description = 'RxJava extension for ObjectBox'
+publishing {
+    publications {
+        getByName<MavenPublication>("mavenJava") {
+            from(components["java"])
+            artifact(sourcesJar)
+            artifact(javadocJar)
+            pom {
+                name.set("ObjectBox RxJava API")
+                description.set("RxJava extension for ObjectBox")
+            }
         }
     }
 }
--- objectbox-rxjava3/build.gradle.kts
+++ objectbox-rxjava3/build.gradle.kts
@@ -1,76 +1,82 @@
-buildscript {
-    ext.javadocDir = file("$buildDir/docs/javadoc")
-}
+import org.jetbrains.dokka.gradle.DokkaTask
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.net.URL
+
+val javadocDir = file("$buildDir/docs/javadoc")
 
 plugins {
     id("java-library")
-    id("kotlin")
+    kotlin("jvm")
     id("org.jetbrains.dokka")
     id("objectbox-publish")
 }
 
 // Note: use release flag instead of sourceCompatibility and targetCompatibility to ensure only JDK 8 API is used.
 // https://docs.gradle.org/current/userguide/building_java_projects.html#sec:java_cross_compilation
-tasks.withType(JavaCompile).configureEach {
+tasks.withType<JavaCompile> {
     options.release.set(8)
 }
 
 // Produce Java 8 byte code, would default to Java 6.
-tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile).configureEach {
+tasks.withType<KotlinCompile> {
     kotlinOptions {
         jvmTarget = "1.8"
     }
 }
 
-tasks.named("dokkaHtml") {
+tasks.named<DokkaTask>("dokkaHtml") {
     outputDirectory.set(javadocDir)
 
-    dokkaSourceSets {
-        configureEach {
-            // Fix "Can't find node by signature": have to manually point to dependencies.
-            // https://github.com/Kotlin/dokka/wiki/faq#dokka-complains-about-cant-find-node-by-signature-
-            externalDocumentationLink {
-                // Point to web javadoc for objectbox-java packages.
-                url.set(new URL("https://objectbox.io/docfiles/java/current/"))
-                // Note: Using JDK 9+ package-list is now called element-list.
-                packageListUrl.set(new URL("https://objectbox.io/docfiles/java/current/element-list"))
-            }
+    dokkaSourceSets.configureEach {
+        // Fix "Can't find node by signature": have to manually point to dependencies.
+        // https://github.com/Kotlin/dokka/wiki/faq#dokka-complains-about-cant-find-node-by-signature-
+        externalDocumentationLink {
+            // Point to web javadoc for objectbox-java packages.
+            url.set(URL("https://objectbox.io/docfiles/java/current/"))
+            // Note: Using JDK 9+ package-list is now called element-list.
+            packageListUrl.set(URL("https://objectbox.io/docfiles/java/current/element-list"))
         }
     }
 }
 
+val kotlinVersion: String by rootProject.extra
+val junitVersion: String by rootProject.extra
+val mockitoVersion: String by rootProject.extra
+
 dependencies {
-    api project(':objectbox-java')
-    api 'io.reactivex.rxjava3:rxjava:3.0.11'
-    compileOnly "org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion"
+    api(project(":objectbox-java"))
+    api("io.reactivex.rxjava3:rxjava:3.0.11")
+    compileOnly("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
 
-    testImplementation "org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion"
-    testImplementation "junit:junit:$junitVersion"
-    testImplementation "org.mockito:mockito-core:$mockitoVersion"
+    testImplementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
+    testImplementation("junit:junit:$junitVersion")
+    testImplementation("org.mockito:mockito-core:$mockitoVersion")
 }
 
-tasks.register('javadocJar', Jar) {
-    dependsOn tasks.named("dokkaHtml")
-    group = 'build'
-    archiveClassifier.set('javadoc')
-    from "$javadocDir"
+val javadocJar by tasks.registering(Jar::class) {
+    dependsOn(tasks.named("dokkaHtml"))
+    group = "build"
+    archiveClassifier.set("javadoc")
+    from(javadocDir)
 }
 
-tasks.register('sourcesJar', Jar) {
-    group = 'build'
-    archiveClassifier.set('sources')
-    from sourceSets.main.allSource
+val sourcesJar by tasks.registering(Jar::class) {
+    group = "build"
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
 }
 
 // Set project-specific properties.
-publishing.publications {
-    mavenJava(MavenPublication) {
-        from components.java
-        artifact sourcesJar
-        artifact javadocJar
-        pom {
-            name = 'ObjectBox RxJava 3 API'
-            description = 'RxJava 3 extensions for ObjectBox'
+publishing {
+    publications {
+        getByName<MavenPublication>("mavenJava") {
+            from(components["java"])
+            artifact(sourcesJar)
+            artifact(javadocJar)
+            pom {
+                name.set("ObjectBox RxJava 3 API")
+                description.set("RxJava 3 extensions for ObjectBox")
+            }
         }
     }
 }
