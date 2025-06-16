4593c549fae75c48994a74d48502388ca37461de
Uwe <noreply+uwe@objectbox.io>
2025-06-16 12:16:36 +0200

    Gradle KTS: convert objectbox-java-api build script

--- objectbox-java-api/build.gradle.kts
+++ objectbox-java-api/build.gradle.kts
@@ -5,30 +5,32 @@ plugins {
 
 // Note: use release flag instead of sourceCompatibility and targetCompatibility to ensure only JDK 8 API is used.
 // https://docs.gradle.org/current/userguide/building_java_projects.html#sec:java_cross_compilation
-tasks.withType(JavaCompile).configureEach {
+tasks.withType<JavaCompile> {
     options.release.set(8)
 }
 
-tasks.register('javadocJar', Jar) {
-    dependsOn javadoc
-    archiveClassifier.set('javadoc')
-    from 'build/docs/javadoc'
+val javadocJar by tasks.registering(Jar::class) {
+    dependsOn(tasks.javadoc)
+    archiveClassifier.set("javadoc")
+    from("build/docs/javadoc")
 }
 
-tasks.register('sourcesJar', Jar) {
-    from sourceSets.main.allSource
-    archiveClassifier.set('sources')
+val sourcesJar by tasks.registering(Jar::class) {
+    from(sourceSets.main.get().allSource)
+    archiveClassifier.set("sources")
 }
 
 // Set project-specific properties.
-publishing.publications {
-    mavenJava(MavenPublication) {
-        from components.java
-        artifact sourcesJar
-        artifact javadocJar
-        pom {
-            name = 'ObjectBox API'
-            description = 'ObjectBox is a fast NoSQL database for Objects'
+publishing {
+    publications {
+        getByName<MavenPublication>("mavenJava") {
+            from(components["java"])
+            artifact(sourcesJar)
+            artifact(javadocJar)
+            pom {
+                name.set("ObjectBox API")
+                description.set("ObjectBox is a fast NoSQL database for Objects")
+            }
         }
     }
 }
