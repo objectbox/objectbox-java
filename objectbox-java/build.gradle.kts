380418691ccff16e261cf66b44f3952a2c94896b
Uwe <noreply+uwe@objectbox.io>
2025-06-16 12:02:50 +0200

    Gradle KTS: convert objectbox-java build script

--- objectbox-java/build.gradle.kts
+++ objectbox-java/build.gradle.kts
@@ -1,3 +1,7 @@
+import kotlin.io.path.appendText
+import kotlin.io.path.readText
+import kotlin.io.path.writeText
+
 plugins {
     id("java-library")
     id("objectbox-publish")
@@ -6,27 +10,26 @@ plugins {
 
 // Note: use release flag instead of sourceCompatibility and targetCompatibility to ensure only JDK 8 API is used.
 // https://docs.gradle.org/current/userguide/building_java_projects.html#sec:java_cross_compilation
-tasks.withType(JavaCompile).configureEach {
+tasks.withType<JavaCompile> {
     options.release.set(8)
 }
 
-ext {
-    javadocForWebDir = "$buildDir/docs/web-api-docs"
-}
+val javadocForWebDir = "$buildDir/docs/web-api-docs"
+val essentialsVersion: String by rootProject.extra
 
 dependencies {
-    api project(':objectbox-java-api')
-    implementation "org.greenrobot:essentials:$essentialsVersion"
-    api 'com.google.code.findbugs:jsr305:3.0.2'
+    api(project(":objectbox-java-api"))
+    implementation("org.greenrobot:essentials:$essentialsVersion")
+    api("com.google.code.findbugs:jsr305:3.0.2")
 
     // https://github.com/spotbugs/spotbugs/blob/master/CHANGELOG.md
-    compileOnly 'com.github.spotbugs:spotbugs-annotations:4.7.3'
+    compileOnly("com.github.spotbugs:spotbugs-annotations:4.7.3")
 }
 
 spotbugs {
-    ignoreFailures = true
-    showStackTraces = true
-    excludeFilter = file("spotbugs-exclude.xml")
+    ignoreFailures.set(true)
+    showStackTraces.set(true)
+    excludeFilter.set(file("spotbugs-exclude.xml"))
 }
 
 tasks.spotbugsMain {
@@ -36,7 +39,7 @@ tasks.spotbugsMain {
 }
 
 // Note: used for the Maven javadoc artifact, a separate task is used to build API docs to publish online
-javadoc {
+tasks.javadoc {
     // Internal Java APIs
     exclude("**/io/objectbox/Cursor.java")
     exclude("**/io/objectbox/KeyValueCursor.java")
@@ -60,19 +63,19 @@ javadoc {
 }
 
 // Note: use packageJavadocForWeb to get as ZIP.
-tasks.register('javadocForWeb', Javadoc) {
-    group = 'documentation'
-    description = 'Builds Javadoc incl. objectbox-java-api classes with web tweaks.'
+tasks.register<Javadoc>("javadocForWeb") {
+    group = "documentation"
+    description = "Builds Javadoc incl. objectbox-java-api classes with web tweaks."
 
-    javadocTool = javaToolchains.javadocToolFor {
+    javadocTool.set(javaToolchains.javadocToolFor {
         // Note: the style changes only work if using JDK 10+, 17 is the LTS release used to publish this
-        languageVersion = JavaLanguageVersion.of(17)
-    }
+        languageVersion.set(JavaLanguageVersion.of(17))
+    })
 
-    def srcApi = project(':objectbox-java-api').file('src/main/java/')
-    if (!srcApi.directory) throw new GradleScriptException("Not a directory: ${srcApi}", null)
+    val srcApi = project(":objectbox-java-api").file("src/main/java/")
+    if (!srcApi.isDirectory) throw GradleException("Not a directory: $srcApi")
     // Hide internal API from javadoc artifact.
-    def filteredSources = sourceSets.main.allJava.matching {
+    val filteredSources = sourceSets.main.get().allJava.matching {
         // Internal Java APIs
         exclude("**/io/objectbox/Cursor.java")
         exclude("**/io/objectbox/KeyValueCursor.java")
@@ -94,80 +97,86 @@ tasks.register('javadocForWeb', Javadoc) {
         exclude("**/io/objectbox/sync/server/JwtConfig.java")
         exclude("**/io/objectbox/sync/server/SyncServerOptions.java")
     }
-    source = filteredSources + srcApi
+    source = filteredSources + fileTree(srcApi)
 
-    classpath = sourceSets.main.output + sourceSets.main.compileClasspath
-    destinationDir = file(javadocForWebDir)
+    classpath = sourceSets.main.get().output + sourceSets.main.get().compileClasspath
+    setDestinationDir(file(javadocForWebDir))
 
-    title = "ObjectBox Java ${version} API"
-    options.overview = "$projectDir/src/web/overview.html"
-    options.bottom = 'Available under the Apache License, Version 2.0 - <i>Copyright &#169; 2017-2025 <a href="https://objectbox.io/">ObjectBox Ltd</a>. All Rights Reserved.</i>'
+    title = "ObjectBox Java ${project.version} API"
+    (options as StandardJavadocDocletOptions).apply {
+        overview = "$projectDir/src/web/overview.html"
+        bottom = "Available under the Apache License, Version 2.0 - <i>Copyright &#169; 2017-2025 <a href=\"https://objectbox.io/\">ObjectBox Ltd</a>. All Rights Reserved.</i>"
+    }
 
     doLast {
         // Note: frequently check the vanilla stylesheet.css if values still match.
-        def stylesheetPath = "$destinationDir/stylesheet.css"
+        val stylesheetPath = "$destinationDir/stylesheet.css"
 
-        // Primary background
-        ant.replace(file: stylesheetPath, token: "#4D7A97", value: "#17A6A6")
+        // Adjust the CSS stylesheet
 
-        // "Active" background
-        ant.replace(file: stylesheetPath, token: "#F8981D", value: "#7DDC7D")
-
-        // Hover
-        ant.replace(file: stylesheetPath, token: "#bb7a2a", value: "#E61955")
-
+        // Change some color values
+        // The stylesheet file should be megabytes at most, so read it as a whole
+        val stylesheetFile = kotlin.io.path.Path(stylesheetPath)
+        val originalContent = stylesheetFile.readText()
+        val replacedContent = originalContent
+            .replace("#4D7A97", "#17A6A6") // Primary background
+            .replace("#F8981D", "#7DDC7D") // "Active" background
+            .replace("#bb7a2a", "#E61955") // Hover
+        stylesheetFile.writeText(replacedContent)
         // Note: in CSS stylesheets the last added rule wins, so append to default stylesheet.
         // Code blocks
-        file(stylesheetPath).append("pre {\nwhite-space: normal;\noverflow-x: auto;\n}\n")
+        stylesheetFile.appendText("pre {\nwhite-space: normal;\noverflow-x: auto;\n}\n")
         // Member summary tables
-        file(stylesheetPath).append(".memberSummary {\noverflow: auto;\n}\n")
+        stylesheetFile.appendText(".memberSummary {\noverflow: auto;\n}\n")
         // Descriptions and signatures
-        file(stylesheetPath).append(".block {\n" +
+        stylesheetFile.appendText(".block {\n" +
                 "    display:block;\n" +
                 "    margin:3px 10px 2px 0px;\n" +
                 "    color:#474747;\n" +
                 "    overflow:auto;\n" +
                 "}")
 
-        println "Javadoc for web created at $destinationDir"
+        println("Javadoc for web created at $destinationDir")
     }
 }
 
-tasks.register('packageJavadocForWeb', Zip) {
-    dependsOn javadocForWeb
-    group = 'documentation'
-    description = 'Packages Javadoc incl. objectbox-java-api classes with web tweaks as ZIP.'
+tasks.register<Zip>("packageJavadocForWeb") {
+    dependsOn("javadocForWeb")
+    group = "documentation"
+    description = "Packages Javadoc incl. objectbox-java-api classes with web tweaks as ZIP."
 
-    archiveFileName = "objectbox-java-web-api-docs.zip"
-    destinationDirectory = file("$buildDir/dist")
+    archiveFileName.set("objectbox-java-web-api-docs.zip")
+    destinationDirectory.set(file("$buildDir/dist"))
 
-    from file(javadocForWebDir)
+    from(file(javadocForWebDir))
 
     doLast {
-        println "Javadoc for web packaged to ${file("$buildDir/dist/objectbox-java-web-api-docs.zip")}"
+        println("Javadoc for web packaged to ${file("$buildDir/dist/objectbox-java-web-api-docs.zip")}")
     }
 }
 
-tasks.register('javadocJar', Jar) {
-    dependsOn javadoc
-    archiveClassifier.set('javadoc')
-    from 'build/docs/javadoc'
+val javadocJar by tasks.registering(Jar::class) {
+    dependsOn("javadoc")
+    archiveClassifier.set("javadoc")
+    from("build/docs/javadoc")
 }
 
-tasks.register('sourcesJar', Jar) {
-    from sourceSets.main.allSource
-    archiveClassifier.set('sources')
+val sourcesJar by tasks.registering(Jar::class) {
+    from(sourceSets.main.get().allSource)
+    archiveClassifier.set("sources")
 }
 
 // Set project-specific properties.
-publishing.publications {
-    mavenJava(MavenPublication) {
-        from components.java
-        artifact sourcesJar
-        artifact javadocJar
-        pom {
-            name = 'ObjectBox Java (only)'
-            description = 'ObjectBox is a fast NoSQL database for Objects'
+publishing {
+    publications {
+        getByName<MavenPublication>("mavenJava") {
+            from(components["java"])
+            artifact(sourcesJar)
+            artifact(javadocJar)
+            pom {
+                name.set("ObjectBox Java (only)")
+                description.set("ObjectBox is a fast NoSQL database for Objects")
+            }
         }
     }
 }
