6761e31dd44bac5c9941e8bf597a987c8c33c24f
Uwe <noreply+uwe@objectbox.io>
2026-03-24 10:30:28 +0100

    BoxStore: compatible with database version 5.3.0 (2026-03-23 or newer)
    2026-03-10 has the JNI API to apply Sync filter variables.

--- objectbox-java/src/main/java/io/objectbox/BoxStore.java
+++ objectbox-java/src/main/java/io/objectbox/BoxStore.java
@@ -92,7 +92,7 @@ public class BoxStore implements Closeable {
      * This is used (currently only in tests) to make sure a database library has a compatible JNI API by checking the
      * version number matches exactly and the date is the same or newer.
      */
-    private static final String VERSION = "5.2.0-2026-03-10";
+    private static final String VERSION = "5.3.0-2026-03-23";
 
     private static final String OBJECTBOX_PACKAGE_NAME = "objectbox";
     private static BoxStore defaultStore;
